cc3de357cf6f9d7ef94c0ed1c7027cb7b2b02412
Beto Esquivel <ppbeto94@gmail.com>
2017-03-20 21:58:22 -0600

    Creates header dumb component inside App file...

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+//import {
+  //BrowserRouter as Router,
+  //Route,
+//} from 'react-router-dom';
+
 import logo from './pililogo.svg';
 import './App.css';
 
@@ -20,54 +25,58 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="hero is-medium is-black is-bold">
-          <div className="hero-head">
-            <header className="nav">
-              <div className="container">
-                <div className="nav-left">
-                  <a className="nav-item">
-                    <img src={logo} className="App-logo" alt="logo" />
-                  </a>
-                </div>
-                <span className="nav-toggle" onClick={this.dropdown.bind(this)}>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                </span>
-                <div
-                  className={
-                    `nav-right nav-menu ${this.state.activeMenu ? 'is-active': ''}`
-                  }>
-                  <a className="nav-item is-active">
-                    Home
-                  </a>
-                  <a className="nav-item">
-                    Examples
-                  </a>
-                  <a className="nav-item">
-                    Documentation
-                  </a>
-                  <span className="nav-item">
-                    <a className="button is-primary is-inverted">
-                      <span className="icon">
-                        <i className="fa fa-github"></i>
-                      </span>
-                      <span>Download</span>
-                    </a>
-                  </span>
-                </div>
-              </div>
-            </header>
-          </div>
-          <div className="hero-body">
-            <h2 className="title">Shorten URLs with Pili</h2>
-          </div>
-        </div>
+        <Header dropdown={this.dropdown.bind(this)} activeMenu={this.state.activeMenu}/>
         <ShortenForm />
         <ShortURLInfo />
       </div>
     );
   }
 }
+
+const Header = (props) => (
+  <div className="hero is-small is-black is-bold">
+    <div className="hero-head">
+      <header className="nav">
+        <div className="container">
+          <div className="nav-left">
+            <a className="nav-item">
+              <img src={logo} className="App-logo" alt="logo" />
+            </a>
+          </div>
+          <span className="nav-toggle" onClick={props.dropdown}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+          <div
+            className={
+              `nav-right nav-menu ${props.activeMenu ? 'is-active': ''}`
+            }>
+            <a className="nav-item is-active">
+              Home
+            </a>
+            <a className="nav-item">
+              Examples
+            </a>
+            <a className="nav-item">
+              Documentation
+            </a>
+            <span className="nav-item">
+              <a className="button is-primary is-inverted">
+                <span className="icon">
+                  <i className="fa fa-github"></i>
+                </span>
+                <span>Download</span>
+              </a>
+            </span>
+          </div>
+        </div>
+      </header>
+    </div>
+    <div className="hero-body">
+      <h2 className="title">Shorten URLs with Pili</h2>
+    </div>
+  </div>
+);
 
 export default App;
